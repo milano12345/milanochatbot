571cb82d5e8e4a1152c3a203cc544e55d408d23b
milano12345 <dmilano857@gmail.com>
2021-01-30 00:35:50 -0700

    added chatbot template

---
+++ chatbot/src/App.js
@@ -0,0 +1,13 @@
+import Chatbot from "react-chatbot-kit";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Chatbot />
+      </header>
+    </div>
+  );
+}
+
+export default App;
